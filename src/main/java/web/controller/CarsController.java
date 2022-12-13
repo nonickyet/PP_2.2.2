a4ac1a488e0f4e3f388f8e0cc3186d3c950ac548
BNHHNnyx <74044198+BNHHNnyx@users.noreply.github.com>
2022-12-13 09:23:47 +0300

    1) Список машин должен инициализироваться в сервисе. Контроллер просит у сервиса его.

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -7,22 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarsService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarsController {
-    private static final List<Car> cars = createCar();
-
-    private static List<Car> createCar() {
-        List<Car> carsBuff = new ArrayList<>();
-        carsBuff.add(new Car("test1", 249, 1600));
-        carsBuff.add(new Car("test2", 249, 1600));
-        carsBuff.add(new Car("test3", 249, 1600));
-        carsBuff.add(new Car("test4", 249, 1600));
-        carsBuff.add(new Car("test5", 249, 1600));
-        return carsBuff;
-    }
+    private static final List<Car> cars = CarsService.getCars();
 
     public static List<Car> getCars() {
         return cars;
@@ -30,7 +19,7 @@ public class CarsController {
 
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam(value = "count", defaultValue = "5") int count) {
-        CarsService.getCars(model, count);
+        CarsService.getCarsByCount(model, count);
         return "cars";
     }
 }
--- src/main/java/web/service/CarsService.java
+++ src/main/java/web/service/CarsService.java
@@ -7,7 +7,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarsService {
-    public static String getCars(ModelMap model, int count) {
+
+    private static List<Car> cars = createCar();
+
+    public static List<Car> getCars() {
+        return cars;
+    }
+
+
+    private static List<Car> createCar() {
+        List<Car> carsBuff = new ArrayList<>();
+        carsBuff.add(new Car("test1", 249, 1600));
+        carsBuff.add(new Car("test2", 249, 1600));
+        carsBuff.add(new Car("test3", 249, 1600));
+        carsBuff.add(new Car("test4", 249, 1600));
+        carsBuff.add(new Car("test5", 249, 1600));
+        return carsBuff;
+    }
+
+    public static String getCarsByCount(ModelMap model, int count) {
         List<Car> result = new ArrayList<>();
         for (int i = 0; i < count && i < 5; i++) {
             result.add(web.controller.CarsController.getCars().get(i));
