b401549729684234615ffe2e34d9d48462eb4736
BNHHNnyx <74044198+BNHHNnyx@users.noreply.github.com>
2022-12-15 19:43:09 +0300

    1) CarsService внедри в контроллер спрингом

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,16 @@ import java.util.List;
 
 @Controller
 public class CarsController {
+    CarsService carsService;
+    // или прям через контекст делать? там прост кфг писать надо будет
+
+    @Autowired
+    public CarsController(CarsService carsService) {
+        this.carsService = carsService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam(value = "count", defaultValue = "5") int count) {
-        CarsService carsService = new CarsService();
         List<Car> cars = carsService.getCarsByCount(count);
         model.addAttribute("cars", cars);
         return "cars";
